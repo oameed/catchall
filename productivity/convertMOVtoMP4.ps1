0c428b112034c11f11a42e31c5f5231b49a07214
oameed <52808211+oameed@users.noreply.github.com>
2025-11-30 17:09:08 -0700

    some changes

--- productivity/convertMOVtoMP4.ps1
+++ productivity/convertMOVtoMP4.ps1
@@ -15,7 +15,8 @@ $fns_output_base   = @($fns_output_base)
 
 for ($i = 0; $i -lt $fns_input.Length; $i++){
     $fn_output     = $path+$fns_output_parent[$i]+"\"+$fns_output_base[$i]+".mp4"
-    ffmpeg -hide_banner -loglevel error -i $fns_input[$i] -c:v mpeg4 -crf 20 -c:a aac $fn_output 
+    ffmpeg -hide_banner -loglevel error -i $fns_input[$i] -c:v libx264 -crf 20 -pix_fmt yuv420p -c:a aac $fn_output 
+   #ffmpeg -hide_banner -loglevel error -i $fns_input[$i] -c:v mpeg4   -q:v 3                   -c:a aac $fn_output 
     }
 
 rm $fns_input
