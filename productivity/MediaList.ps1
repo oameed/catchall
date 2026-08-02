2278ca051cee5159e43eadc0d3744cf7511a5326
oameed <52808211+oameed@users.noreply.github.com>
2026-08-02 10:29:14 -0600

    some changes

--- productivity/MediaList.ps1
+++ productivity/MediaList.ps1
@@ -9,18 +9,17 @@ param([string]$m       = "all" ,
 
 $PlaylistName      = 'playlist.m3u'
 
-$ExtensionsExclude = @('ps1','srt','json','md')
+$ExtensionsExclude = @('*.ps1','*.srt','*.json','*.md')
 
 $ExtensionsInclude = @{ all = '*'
                         vid = @('*.MP4','*.mp4','*.MOV','*.mov')
                         img = @('*.heic','*.jpg','*.png')        }
 
-$filenames         = Get-ChildItem  -File -Name -Recurse -Include $ExtensionsInclude[$m]
-$filenames         = $filenames | Where-Object {$_.Split('.')[-1] -notin $ExtensionsExclude}
+$filenames = Get-ChildItem  -File -Name -Recurse -Include $ExtensionsInclude[$m] -Exclude $ExtensionsExclude
 
 if ($shuffle -eq $true)
 {
-      $filenames   = $filenames | Get-Random -Shuffle
+      $filenames = $filenames | Get-Random -Shuffle
 }
 
 echo $filenames > $PlaylistName
